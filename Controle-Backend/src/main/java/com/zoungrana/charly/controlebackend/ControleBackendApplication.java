19779313ea89f986718979c0452d7e1ce6c490fc
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-05-11 09:01:21 +0100

    First Commit

---
+++ Controle-Backend/src/main/java/com/zoungrana/charly/controlebackend/ControleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zoungrana.charly.controlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleBackendApplication.class, args);
+    }
+
+}
